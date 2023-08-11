11a3eb74cc032a8976a82a0301690792440553f9
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-08-11 10:37:44 +0200

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,5 +9,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent {
     loginForm = new FormGroup({
         name: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        age: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        repeatPassword: new FormControl('', [Validators.required]),
     });
 }
