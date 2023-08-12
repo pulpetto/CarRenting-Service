9046570f16c470d9256221bdd4c528c5f053789a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-08-12 09:19:33 +0200

    refactoring input name

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,8 +10,11 @@ export class LoginComponent {
     loginForm = new FormGroup({
         name: new FormControl('', [Validators.required]),
         lastName: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.required]),
-        age: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        age: new FormControl('', [
+            Validators.required,
+            Validators.pattern('^[0-9]*$'),
+        ]),
         password: new FormControl('', [Validators.required]),
         repeatPassword: new FormControl('', [Validators.required]),
     });
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.css'],
 })
 export class InputComponent {
-
+    @Input() control!: FormControl;
 }
