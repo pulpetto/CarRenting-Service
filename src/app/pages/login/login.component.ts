0a85709bfc34ef7a21547a12fe52eecd4173b8a7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-08-15 21:09:11 +0200

    user login logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import {
-    FormGroup,
-    FormControl,
-    Validators,
-    AbstractControl,
-    ValidationErrors,
-} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
     selector: 'app-login',
@@ -13,9 +10,41 @@ import {
     styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+    signinPromptVisibility!: boolean;
+
+    constructor(private userService: UserService, private router: Router) {
+        this.signinPromptVisibility = false;
+    }
+
     loginForm = new FormGroup({
         username: new FormControl('', [Validators.required]),
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required]),
     });
+
+    onLogIn() {
+        if (
+            this.userService
+                .getUsers()
+                .some(
+                    (user) =>
+                        user.username === this.loginForm.get('username')?.value!
+                ) &&
+            this.userService
+                .getUsers()
+                .some(
+                    (user) => user.email === this.loginForm.get('email')?.value!
+                ) &&
+            this.userService
+                .getUsers()
+                .some(
+                    (user) =>
+                        user.password === this.loginForm.get('password')?.value!
+                )
+        ) {
+            this.router.navigate(['/home']);
+        } else {
+            this.signinPromptVisibility = true;
+        }
+    }
 }
